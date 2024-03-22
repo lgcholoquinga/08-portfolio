99115334be8175188610967a2a4768561cfeced2
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-03-21 22:08:10 -0500

    create srtucture

---
+++ src/app/pages/portfolio/portfolio-layout/portfolio-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-portfolio-layout',
+  standalone: true,
+  imports: [],
+  templateUrl: './portfolio-layout.component.html',
+  styleUrl: './portfolio-layout.component.scss'
+})
+export class PortfolioLayoutComponent {
+
+}
