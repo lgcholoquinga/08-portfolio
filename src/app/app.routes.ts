08716337904ca551529e4064be03904ccc0d9ae5
lgcholoquinga <luisg.choloquinga@gamil.com>
2024-03-21 22:21:13 -0500

    config routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'portfolio',
+        loadChildren: () => import('./pages/portfolio/portfolio.routes').then(r => r.PORTFOLIO_ROUTES)
+    },
+    {
+        path: '',
+        redirectTo: '/portfolio',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: '/portfolio',
+    }
+];
--- src/app/pages/portfolio/portfolio.routes.ts
+++ src/app/pages/portfolio/portfolio.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from "@angular/router";
 
-export const PORTFOLIO_ROUTES: Routes = [];
+export const PORTFOLIO_ROUTES: Routes = [
+    {
+        path: '',
+        loadComponent: () => import('./portfolio-layout/portfolio-layout.component').then(c => c.PortfolioLayoutComponent),
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./home/home.component').then(c => c.HomeComponent)
+            }
+        ]
+    }
+];
